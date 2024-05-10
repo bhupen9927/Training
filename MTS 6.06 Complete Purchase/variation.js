9affea8964a4a53a56797022b105b1f0c61f59e8
bhupen9927 <bhupendrasingh32709@gmail.com>
2024-05-10 21:27:54 +0530

    MTS Test

---
+++ MTS 6.06 Complete Purchase/variation.js
@@ -0,0 +1,137 @@
+/*fecli-injected JavaScript*/
+  ;(function(){
+    var style=document.createElement('style');style.type='text/css';style.appendChild(document.createTextNode('html body .payment_methods {    position: relative;    display: flex;    flex-direction: row;    grid-gap: 15px;}html body .payment_methods>.wc_payment_method>div {    position: absolute;    width: 100% !important;    left: 0px;}html body .payment_methods>.payment_method_wizit {    display: block !important;}html body .payment_methods .circle-check,.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p [for="bpoint-card-holder-name"],.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p [for="bpoint-card-number"],html body .cart-summary h3,html body .cart-summary .cart-summary-content,html body .cart-summary .other-details {    display: none !important;}html body .payment_methods>.wc_payment_method>label {    border-radius: 30px !important;    border: 3px solid #E3E3E3 !important;    background: #FFF !important;    padding: 26px 20px !important;    display: flex !important;    flex-direction: column;}html body .payment_methods>.payment_method_bpoint>label {    padding: 18.9px 13.22px !important;}html body .payment_methods>.payment_method_bpoint>.label::before {    content: \'\';    background-image: url(https://i.ibb.co/2hmLvcX/credit-card.png);    width: 40px;    height: 32px;    background-size: cover;}html body .wc_payment_methods>.payment_method_bpoint,html body .wc_payment_methods>.payment_method_zipmoney {    order: -1 !important;}html body .wc_payment_methods>.payment_method_bpoint>.payment_box {    background: transparent !important;    margin-top: 10px !important;}.instant-checkout .inner-box .steps .step.step-4 .content .payment-form .wc_payment_methods .wc_payment_method .payment_box.payment_method_bpoint .input-text {    padding: 17px 16px 17px !important;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:first-of-type::before {    content: \'Creadit Card Secured by Commonwealth\';    font-size: 16px !important;    color: #333333;    font-weight: 600;}html body .eg-cardname {    font-size: 12px !important;    font-weight: 600;    margin-bottom: 10px;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:first-of-type {    margin-bottom: 20px !important;}html body .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(3)::before {    content: \'\';    top: 44px !important;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(4) label {    font-weight: 600;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(2),.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(3),.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(4) {    margin-bottom: 20px;}html body .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(6) label {    top: 48px !important;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:last-of-type {    top: 20px !important;}html body .step-4 .content {    display: flex;    flex-direction: column;}html body .wc_payment_methods>.payment_method_bpoint {    height: 684px !important;}.eg-remove .wc_payment_methods>.payment_method_bpoint {    height: 435px !important;}html body .cart-summary {    width: 100% !important;    max-width: 100% !important;    background: #FFFFFF !important;    padding-top: 0px !important;}.step-4 .content .cart-summary .woocommerce-terms-and-conditions-wrapper {    margin-top: 0px !important;}.eg-show .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(6) label {    top: 40px !important;}.instant-checkout .inner-box .steps .step.step-4 .content .cart-summary .place-order button {    background: #ddd;    color: #000;    position: absolute;    bottom: 35px;    right: 55px;    opacity: 1;}.eg-show .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(6) input {    padding-top: 22px !important;    padding-bottom: 12px !important;}.step-4 .content .cart-summary .coupon-container {    margin-top: 5px !important;    z-index: 9999;}.step-4 .content .cart-summary [type="submit"] {    width: fit-content !important;    color: #333333;}.content .nav-buttons {    width: 77% !important;}html body .content .payment-form {    padding-bottom: 10px !important;}html body .content .cart-summary {    padding-left: 55px !important;    padding-bottom: 75px !important;}html body .content .nav-buttons {    border: unset !important;}html body .content .coupon-container {    border-bottom: 1px solid #e6e6e6 !important;    margin-bottom: 30px !important;    padding-bottom: 20px !important;}html body .content .nav-buttons {    padding-left: 0px !important;    position: absolute;    bottom: 25px;    left: 108px;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(4) {    width: unset !important;}.step-4 .payment_method_bpoint .payment_box {    padding-left: 0px !important;}.wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(5) {    width: 187px !important;}html body .steps .step-4 {    width: 58%;    margin: auto;    position: relative;}.inner-box .cart-content {    right: 235px !important;    z-index: 9;}@media only screen and (max-width:1024px) {    .wc_payment_methods .wc_payment_method>label {        width: 147px !important;        height: 110px !important;        display: flex;        justify-content: center;        align-items: center;    }    .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:first-of-type::before {        margin-bottom: 10px !important;    }    html body .wc_payment_methods>.payment_method_zipmoney {        height: 225px !important;    }    html body .wc_payment_methods>.payment_method_afterpay {        height: 460px !important;    }    .inner-box .cart-content {        right: 0px !important;    }    html body .steps .step-4 {        width: 100%;    }    html body .eg-cardname {        font-size: 14px !important;    }    html body .wc_payment_methods>.payment_method_bpoint {        height: 750px !important;    }    html body .mobile-actions {        z-index: 99999;    }    html body .content .cart-summary {        padding-left: 15px !important;        padding-bottom: 80px !important;    }}@media only screen and (max-width:767px) {    html body .payment_methods>.payment_method_bpoint>label {        padding: 17.5px 17.5px !important;    }}@media only screen and (max-width:462px) {    .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(5) {        margin-bottom: 25px !important;    }    .wc_payment_methods>.payment_method_bpoint #bpoint-cc-form>p:nth-child(5) label {        display: none !important;    }    html body .wc_payment_methods>.payment_method_bpoint {        height: 985px !important;    }}@media only screen and (max-width:660px) {    .wc_payment_methods .wc_payment_method .payment_box {        padding-top: 155px !important;    }    .payment_methods>.payment_method_wizit .payment_box {        padding-top: 25px !important;        margin-top: 150px !important;    }    .wc_payment_methods .payment_method_afterpay label {        position: absolute !important;        top: 125px;        left: 0px;    }}@media only screen and (max-width:375px) {    html body .wc_payment_methods>.payment_method_bpoint {        height: 955px !important;    }}@media only screen and (min-width:377px) and (max-width:462px) {    html body .wc_payment_methods>.payment_method_bpoint {        height: 1000px !important;    }}@media only screen and (max-width:377px) {    html body .wc_payment_methods>.payment_method_bpoint {        height: 1020px !important;    }}@media only screen and (min-width:462px) and (max-width:500px) {    html body .wc_payment_methods>.payment_method_bpoint {        height: 900px !important;    }}html body .place-order-button:not([disabled]) {    background: #509e31 !important;    color: #fff !important;}@media only screen and (max-width:660px) {    .wc_payment_methods .payment_method_afterpay .payment_box {        padding-top: 20px !important;        margin-top: 260px !important;    }    .payment_methods>.payment_method_wizit label {        margin-left: -15px !important;    }    .wc_payment_methods .payment_method_zipmoney .payment_box {        padding-top: 25px !important;        margin-top: 150px !important;    }    .eg-remove .wc_payment_methods>.payment_method_bpoint {        height: 590px !important;    }}@media only screen and (min-width:1700px) {    html body .payment_methods>.payment_method_bpoint>label {        padding: 16.2px 13.23px !important;    }    html body .payment_methods>.payment_method_afterpay>label,    html body .payment_methods>.payment_method_zipmoney>label {        padding: 26px 31.36px !important;    }}@media only screen and (min-width:500px) and (max-width:660px) {    html body .wc_payment_methods>.payment_method_bpoint {        height: 885px !important;    }}@media only screen and (max-width:552px) {    .payment_methods>.payment_method_wizit label {        position: absolute !important;        top: 125px;        left: 160px;    }    .payment_methods>.payment_method_wizit label {        margin-left: unset !important;    }    .payment_methods>.payment_method_wizit .payment_box {        padding-top: 25px !important;        margin-top: 260px !important;    }}'));document.head.appendChild(style);
+(function () {
+  try {
+    /* main variables */
+    var debug = 0;
+    var variation_name = "";
+    var $;
+    /* all Pure helper functions */
+
+    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+      var interval = setInterval(function () {
+        if (
+          document &&
+          document.querySelector(selector) &&
+          document.querySelectorAll(selector).length > 0
+        ) {
+          clearInterval(interval);
+          trigger();
+        }
+      }, delayInterval);
+      setTimeout(function () {
+        clearInterval(interval);
+      }, delayTimeout);
+    }
+
+    function live(selector, event, callback, context) {
+      function addEvent(el, type, handler) {
+        if (el.attachEvent) el.attachEvent('on' + type, handler);
+        else el.addEventListener(type, handler);
+      }
+      this.Element &&
+        (function (ElementPrototype) {
+          ElementPrototype.matches =
+            ElementPrototype.matches ||
+            ElementPrototype.matchesSelector ||
+            ElementPrototype.webkitMatchesSelector ||
+            ElementPrototype.msMatchesSelector ||
+            function (selector) {
+              var node = this,
+                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                i = -1;
+              while (nodes[++i] && nodes[i] != node);
+              return !!nodes[i];
+            };
+        })(Element.prototype);
+      function live(selector, event, callback, context) {
+        addEvent(context || document, event, function (e) {
+          var found,
+            el = e.target || e.srcElement;
+          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+          if (found) callback.call(el, e);
+        });
+      }
+      live(selector, event, callback, context);
+    };
+
+    function live(selector, event, callback, context) {
+      function addEvent(el, type, handler) {
+        if (el.attachEvent) el.attachEvent('on' + type, handler);
+        else el.addEventListener(type, handler);
+      }
+      this.Element &&
+        (function (ElementPrototype) {
+          ElementPrototype.matches =
+            ElementPrototype.matches ||
+            ElementPrototype.matchesSelector ||
+            ElementPrototype.webkitMatchesSelector ||
+            ElementPrototype.msMatchesSelector ||
+            function (selector) {
+              var node = this,
+                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                i = -1;
+              while (nodes[++i] && nodes[i] != node);
+              return !!nodes[i];
+            };
+        })(Element.prototype);
+      function live(selector, event, callback, context) {
+        addEvent(context || document, event, function (e) {
+          var found,
+            el = e.target || e.srcElement;
+          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+          if (found) callback.call(el, e);
+        });
+      }
+      live(selector, event, callback, context);
+    };
+
+    live('.step-3 .nav-buttons [type="button"]', 'click', function () {
+      init();
+    });
+
+    live('.wc_payment_methods > .payment_method_bpoint', 'click', function () {
+      document.body.classList.remove('eg-remove');
+    });
+
+    live('.wc_payment_methods > .payment_method_afterpay , .wc_payment_methods > .payment_method_zipmoney', 'click', function () {
+      document.body.classList.add('eg-remove');
+    });
+
+    function init() {
+      document.querySelector('#order_review > h4').innerText = 'Select payment type:';
+      document.querySelector('html body .payment_methods > .payment_method_bpoint > .label').innerText = 'Credit/debit';
+      let cardname = `<span class= "eg-cardname">Cardholder name <span class="required">*</span></span>`
+      document.querySelector('.wc_payment_methods > .payment_method_bpoint #bpoint-cc-form > p:nth-child(2)').insertAdjacentHTML('afterbegin', cardname);
+
+      let cardnumber = `<span class= "eg-cardname">Card number<span class="required">*</span></span>`
+      document.querySelector('.wc_payment_methods > .payment_method_bpoint #bpoint-cc-form > p:nth-child(3)').insertAdjacentHTML('afterbegin', cardnumber);
+
+      let cvn = `<span class= "eg-cardname">CVN<span class="required">*</span></span>`
+      document.querySelector('.wc_payment_methods > .payment_method_bpoint #bpoint-cc-form > p:nth-child(6)').insertAdjacentHTML('afterbegin', cvn);
+      document.querySelector('.wc_payment_methods > .payment_method_bpoint #bpoint-cc-form > p:nth-child(6) label').innerText = 'Card Verification Number';
+
+      var inputField = document.querySelector('.wc_payment_methods > .payment_method_bpoint #bpoint-cc-form > p:nth-child(6) input');
+      function toggleClassBasedOnInputLength() {
+        if (inputField.value.length > 0) {
+          document.body.classList.add('eg-show');
+        } else {
+          document.body.classList.remove('eg-show');
+        }
+      }
+
+      inputField.addEventListener('input', toggleClassBasedOnInputLength);
+      window.addEventListener('load', toggleClassBasedOnInputLength);
+    }
+
+    waitForElement('#order_review > h4', init, 50, 15000);
+  } catch (e) {
+    if (debug) console.log(e, "error in Test" + variation_name);
+  }
+})();
+
+
+
+})();
+/*fecli-injected JavaScript*/
