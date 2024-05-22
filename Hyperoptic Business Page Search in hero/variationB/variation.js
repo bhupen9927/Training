71ae877ec8f9ddf20549bcdf10a91aee3468ed1c
bhupen9927 <bhupendrasingh32709@gmail.com>
2024-05-22 16:36:32 +0530

    updated

--- Hyperoptic Business Page Search in hero/variationB/variation.js
+++ Hyperoptic Business Page Search in hero/variationB/variation.js
@@ -21,7 +21,40 @@
       }, delayTimeout);
     }
 
-
+    function live(selector, event, callback, context) {
+      /****Helper Functions****/
+      // helper for enabling IE 8 event bindings
+      function addEvent(el, type, handler) {
+        if (el.attachEvent) el.attachEvent("on" + type, handler);
+        else el.addEventListener(type, handler);
+      }
+      // matches polyfill
+      this.Element &&
+        (function (ElementPrototype) {
+          ElementPrototype.matches =
+            ElementPrototype.matches ||
+            ElementPrototype.matchesSelector ||
+            ElementPrototype.webkitMatchesSelector ||
+            ElementPrototype.msMatchesSelector ||
+            function (selector) {
+              var node = this,
+                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                i = -1;
+              while (nodes[++i] && nodes[i] != node);
+              return !!nodes[i];
+            };
+        })(Element.prototype);
+      // live binding helper using matchesSelector
+      function live(selector, event, callback, context) {
+        addEvent(context || document, event, function (e) {
+          var found,
+            el = e.target || e.srcElement;
+          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+          if (el && found) callback.call(el, e);
+        });
+      }
+      live(selector, event, callback, context);
+    }
     //wait for swiper 
     function waitForSwiper(trigger) {
       var interval = setInterval(function () {
@@ -38,17 +71,42 @@
 
     /* Variation Init */
     function init() {
-      document.querySelector('.hero-block-desktop table td > div:last-child').insertAdjacentElement('afterend', document.querySelector('.show-desktop .elastic-search.compact'));
-
-      document.querySelector('.hero-block-mobile table td > div:last-child').insertAdjacentElement('afterend', document.querySelector('.show-mobile .elastic-search.compact.white'));
-      
-
+      var searchEle = document.querySelector('.modal-dialog #modalElastic');
+      searchEle.classList.add('eg-search');
+      document.querySelector('.hero-block-desktop table td > div:last-child').insertAdjacentElement('afterend', searchEle);
+      live('.button-wr button', 'mousedown', function () {
+        console.log('ssd');
+        searchEle = document.querySelector('.eg-search');
+        document.querySelector('div[id^="orderNowElasticModalblock_"] .modal-dialog p').insertAdjacentElement('afterend', searchEle);
+      });
+      live('div[id^="orderNowElasticModalblock_"] .modal-dialog button.close', 'mousedown', function () {
+        searchEle = document.querySelector('.eg-search');
+        document.querySelector('.hero-block-desktop table td > div:last-child').insertAdjacentElement('afterend', searchEle);
+      });
     }
 
 
+    function initM() {
+      
+      var searchEle = document.querySelector('.modal-dialog #modalElastic');
+      searchEle.classList.add('eg-search');
+       document.querySelector('.hero-block-mobile table td > div:last-child').insertAdjacentElement('afterend', searchEle);
+      live('.button-wr button', 'mousedown', function () {
+        console.log('ssd');
+        searchEle = document.querySelector('.eg-search');
+        document.querySelector('div[id^="orderNowElasticModalblock_"] .modal-dialog p').insertAdjacentElement('afterend', searchEle);
+      });
+      live('div[id^="orderNowElasticModalblock_"] .modal-dialog button.close', 'mousedown', function () {
+        searchEle = document.querySelector('.eg-search');
+        document.querySelector('.hero-block-mobile table td > div:last-child').insertAdjacentElement('afterend', searchEle);
+      });
+    }
+
 
     /* Initialise variation */
-    waitForElement(".hero-block-desktop table td > div:last-child, .hero-block-mobile table td > div:last-child", init, 50, 15000);
+    waitForElement(".modal-dialog #modalElastic", init, 500, 15000);
+    waitForElement(".modal-dialog #modalElastic", initM, 500, 15000);
+
   } catch (e) {
     if (debug) console.log(e, "error in Test" + variation_name);
   }
