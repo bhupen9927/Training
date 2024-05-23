dca594b46495aed1cfb61f8485eafc775ddfc2d2
bhupen9927 <bhupendrasingh32709@gmail.com>
2024-05-23 17:38:32 +0530

    updated

---
+++ Demazin 4.02 Featured Products Below Fold/readme.md
@@ -0,0 +1,1 @@
+v1 and v2 are the same. The difference in v2 is that we made it for this page. (https://difflam.com.au/)
---
+++ Demazin 4.02 Featured Products Below Fold/share.js
@@ -0,0 +1,74 @@
+(function () {
+  try {
+    /* main variables */
+    var debug = 0;
+    var variation_name = "";
+    var $;
+    /* all Pure helper functions */
+
+    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+      var interval = setInterval(function () {
+        if (
+          document &&
+          document.querySelector(selector) &&
+          document.querySelectorAll(selector).length > 0
+        ) {
+          clearInterval(interval);
+          trigger();
+        }
+      }, delayInterval);
+      setTimeout(function () {
+        clearInterval(interval);
+      }, delayTimeout);
+    }
+
+    function live(selector, event, callback, context) {
+      function addEvent(el, type, handler) {
+        if (el.attachEvent) el.attachEvent('on' + type, handler);
+        else el.addEventListener(type, handler);
+      }
+      this.Element &&
+        (function (ElementPrototype) {
+          ElementPrototype.matches =
+            ElementPrototype.matches ||
+            ElementPrototype.matchesSelector ||
+            ElementPrototype.webkitMatchesSelector ||
+            ElementPrototype.msMatchesSelector ||
+            function (selector) {
+              var node = this,
+                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                i = -1;
+              while (nodes[++i] && nodes[i] != node);
+              return !!nodes[i];
+            };
+        })(Element.prototype);
+      function live(selector, event, callback, context) {
+        addEvent(context || document, event, function (e) {
+          var found,
+            el = e.target || e.srcElement;
+          while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+          if (found) callback.call(el, e);
+        });
+      }
+      live(selector, event, callback, context);
+    };
+
+
+    /* Variation Init */
+    function init() {
+      live('[id*="tpf_featured_product"] .owl-item', 'click', function () {
+        console.log('featured Product clicked');
+      });
+      live('html body .woosingle__retailer .woosingle__retailer--button', 'click', function () {
+        console.log('Clicks "Buy Now" on any product page');
+      });
+    }
+
+    /* Initialize variation */
+    waitForElement('html body', init, 50, 15000);
+  } catch (e) {
+    if (debug) console.log(e, "error in Test" + variation_name);
+  }
+})();
+
+
