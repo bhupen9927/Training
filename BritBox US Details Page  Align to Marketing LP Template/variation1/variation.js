1d7b2d977e34f03ba39f9f5414599c64ae6f8b9f
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-05-24 20:53:08 +0530

    updated

--- BritBox US Details Page  Align to Marketing LP Template/variation1/variation.js
+++ BritBox US Details Page  Align to Marketing LP Template/variation1/variation.js
@@ -24,17 +24,33 @@
 
 
     const heroSection=
-        `<div class="eg-container">
-            <div clas="eg-hero-section">
-                <div class="eg-hero-container">
-                    <div class="eg-bg-paradise">
-                      <img class="eg-hero-season" src="https://i.ibb.co/PcDy4r4/season13-Sticky.png">
-                      <a class="eg-watch-cta" href="#"> Start Watching Now</a>
-                      <p class="eg-hero-content">Free 7-day trial, then just $8.99/month or $89.99/year</p>
-                    </div>
-                </div>
+        `<div class="eg-hero-container">
+          <div class="eg-bg-paradise">
+            <div class="eg-hero-wrapper">
+              <a class="eg-watch-cta" href="#"> Start Watching Now</a>
+              <p class="eg-hero-content">Free 7-day trial, then just $8.99/month or $89.99/year</p>
             </div>
-        </div>`
+          </div>
+        </div>
+        
+        <div class="eg-image-container">
+          <div class="eg-image-gallery">
+            <div class="eg-img-wrapper eg-left-images">
+              <img class="eg-big-img eg-bigls-img eg-bigup-img" src="https://i.ibb.co/wzr0LjM/Image-13.png">
+              <div class="eg-rg-imgs">
+                <img class="eg-sm-img eg-smup-img" src="https://i.ibb.co/r6gqfqk/Image-14.png">
+                <img class="eg-sm-img" src="https://i.ibb.co/yQq3CJb/Image-15.png">
+              </div>
+            </div>
+            <div class="eg-img-wrapper eg-right-images">
+              <img class="eg-big-img eg-bigls-img eg-biglw-img" src="https://i.ibb.co/PMTp2zM/Image-16.png">
+              <div class="eg-rg-imgs">
+                <img class="eg-sm-img eg-smup-img" src="https://i.ibb.co/d2rHCCh/Image-17.png">
+                <img class="eg-sm-img" src="https://i.ibb.co/qN9L39g/Image-18.png">
+              </div>
+            </div>
+          </div>
+        </div>`;
 
 
     /* Variation Init */
