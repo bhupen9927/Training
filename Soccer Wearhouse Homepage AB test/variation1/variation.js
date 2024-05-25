9a03fb9cc68ebf9c3891396f94838f040713bf18
Bhupendra Singh <108919397+bhupen9927@users.noreply.github.com>
2024-05-26 01:00:28 +0530

    added

--- Soccer Wearhouse Homepage AB test/variation1/variation.js
+++ Soccer Wearhouse Homepage AB test/variation1/variation.js
@@ -26,28 +26,28 @@
         `<div class="eg-faq-section">
             <div class="eg-faq-container">
                 <h2 class="eg-faq-hd" >frequently asked questions</h2>
-                <div class="accordion">
-                    <div class="accordion-item">
-                    <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
-                    <div class="accordion-content">
+                <div class="eg-accordion">
+                    <div class="eg-accordion-item">
+                    <button aria-expanded="false"><span class="eg-accordion-title">Why is the moon sometimes out during the day?</span><span class="eg-icon" aria-hidden="true"></span></button>
+                    <div class="eg-accordion-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
                     </div>
                     </div>
-                    <div class="accordion-item">
-                    <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
-                    <div class="accordion-content">
+                    <div class="eg-accordion-item">
+                    <button aria-expanded="false"><span class="eg-accordion-title">Why is the sky blue?</span><span class="eg-icon" aria-hidden="true"></span></button>
+                    <div class="eg-accordion-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
                     </div>  
                     </div>
-                    <div class="accordion-item">
-                    <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
-                    <div class="accordion-content">
+                    <div class="eg-accordion-item">
+                    <button aria-expanded="false"><span class="eg-accordion-title">Will we ever discover aliens?</span><span class="eg-icon" aria-hidden="true"></span></button>
+                    <div class="eg-accordion-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
                     </div>
                     </div>
-                    <div class="accordion-item">
-                    <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
-                    <div class="accordion-content">
+                    <div class="eg-accordion-item">
+                    <button aria-expanded="false"><span class="eg-accordion-title">How much does the Earth weigh?</span><span class="eg-icon" aria-hidden="true"></span></button>
+                    <div class="eg-accordion-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
                     </div>
                     </div>
@@ -63,7 +63,7 @@
         if(footer){
             footer.insertAdjacentHTML('beforebegin', FAQ);
         }
-        const items = document.querySelectorAll(".accordion button");
+        const items = document.querySelectorAll(".eg-accordion button");
         function toggleAccordion() {
         const itemToggle = this.getAttribute('aria-expanded');
         for (i = 0; i < items.length; i++) {
